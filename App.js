3974317a64127debc8778a307260f81821fff4ad
xhy <695729033@qq.com>
2018-01-08 17:51:49 +0800

    TEST

--- App.js
+++ App.js
@@ -27,7 +27,7 @@ export default class App extends Component<{}> {
           Welcome to React Native!
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit App.js
+          To get started, edit App.js666
         </Text>
         <Text style={styles.instructions}>
           {instructions}
