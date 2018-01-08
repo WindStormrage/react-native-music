44a1e4a7264c261357bc441477a4b75bd2a984c6
xhy <695729033@qq.com>
2018-01-08 18:03:28 +0800

    TEST

---
+++ src/App.js
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  View
+} from 'react-native';
+
+
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <View>
+				hello
+      </View>
+    );
+  }
+}
+
